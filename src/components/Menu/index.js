7a90038307106dd26f4732ed1605fceb650e26ea
Marcela Barcelos <109169959+BBmon3890@users.noreply.github.com>
2023-02-07 21:29:39 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
-
 const StyledMenu = styled.header`
   display: flex;
   flex-direction: row;
